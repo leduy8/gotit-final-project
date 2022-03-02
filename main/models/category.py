d503e578dcffcad81390e1d2ec060852b9da3eee
Duy Le <duy.leduc8@gmail.com>
2022-03-02 14:25:43 +0700

    feat: Build category model

---
+++ main/models/category.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from main import db
+
+
+class CategoryModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated = db.Column(db.DateTime, default=datetime.utcnow,
+                        onupdate=datetime.utcnow, nullable=False)
+
+    def __str__(self) -> str:
+        return f'<CategoryModel {self.id} {self.name}>'
