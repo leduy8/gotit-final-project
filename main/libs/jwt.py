8d9834b06a4c951b017e4a342b84701c827733a5
Duy Le <duy.leduc8@gmail.com>
2022-03-02 15:24:15 +0700

    feat: Build jwt module for authorization

---
+++ main/libs/jwt.py
@@ -0,0 +1,26 @@
+from typing import Dict
+import jwt
+from flask import request
+
+from main import config
+from main.commons.exceptions import Unauthorized
+
+
+def create_access_token(identity) -> str:
+    return jwt.encode(payload={'id': identity}, key=config.SECRET_KEY, algorithm='HS256')
+
+
+def get_jwt_token() -> str:
+    auth_header = request.headers.get('Authorization', None)
+
+    if not auth_header or not auth_header.startswith('Bearer ') or len(auth_header.split(' ')) != 2:
+        raise Unauthorized()
+
+    return auth_header.split(' ')[1]
+
+
+def get_jwt_payload(token: str) -> Dict:
+    try:
+        jwt.decode(token, config.SECRET_KEY, algorithms=['HS256'])
+    except jwt.DecodeError:
+        return None
