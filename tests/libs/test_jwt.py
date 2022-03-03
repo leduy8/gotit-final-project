de8a1c53dfe854def8a1007059e09db4d2e8e8a4
Duy Le <duy.leduc8@gmail.com>
2022-03-03 12:31:26 +0700

    test: Test authenticate logic

--- tests/libs/test_jwt.py
+++ tests/libs/test_jwt.py
@@ -1,19 +1,43 @@
 import pytest
 
 from main.libs.jwt import create_access_token, get_jwt_payload
+from main.models.user import UserModel
 
 
+@pytest.mark.parametrize(
+    'payload',
+    [
+        {'id': 1},
+        {'id': 2, 'name': 'Duy', 'age': 23}
+    ]
+)
 def test_success_create_access_token(payload):
-    assert 1 == 1
+    token = create_access_token(payload)
+    assert type(token) == str
+    assert len(token.split('.')) == 3
 
 
+@pytest.mark.parametrize(
+    'payload',
+    [
+        1,
+        'abc',
+        1.2,
+        [1, 2, 3],
+        {1, 2, 3},
+        (1, 2),
+        UserModel()
+    ]
+)
 def test_fail_create_access_token(payload):
-    assert 1 == 1
+    token = create_access_token(payload)
+    assert token is None
 
 
 def test_success_get_jwt_payload():
     token = create_access_token({'id': 123})
     assert get_jwt_payload(token) is not None
+    assert type(get_jwt_payload(token)) == dict
 
 
 def test_fail_get_jwt_payload():
