cfaa9dfb1be94aa151771e92bcebcd01a4e1d531
Duy Le <duy.leduc8@gmail.com>
2022-03-07 15:25:14 +0700

    fix: Add relationship between User and Category

--- main/schemas/category.py
+++ main/schemas/category.py
@@ -9,5 +9,6 @@ class CategorySchema(BaseSchema):
         validate=validate.Length(max=50),
         required=True
     )
+    user_id = fields.Integer(dump_only=True)
     created = fields.DateTime(dump_only=True)
     updated = fields.DateTime(dump_only=True)
