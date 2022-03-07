f0a22fd9596096cdd90868d04093013bccc2a8fc
Duy Le <duy.leduc8@gmail.com>
2022-03-07 13:51:54 +0700

    refactor: Move user creation to user engine

--- main/controllers/user.py
+++ main/controllers/user.py
@@ -1,12 +1,10 @@
 from flask import jsonify, request
 from marshmallow import ValidationError
 
-from main import app, db
-from main.models.user import UserModel
+from main import app
 from main.schemas.user import UserSchema
 from main.commons.exceptions import BadRequest
-from main.engines.user import find_by_email
-from main.libs.password_utils import gen_salt, generate_password_hash
+from main.engines.user import find_by_email, create_user
 from main.libs.jwt import create_access_token
 
 
@@ -23,16 +21,6 @@ def register_user():
     if find_by_email(data['email']):
         raise BadRequest(error_data=data, error_message=f'Email is already registered.')
 
-    salt = gen_salt()
-    password_hash = generate_password_hash(data['password'], salt)
-
-    user = UserModel(
-        email=data['email'],
-        password_hash=password_hash,
-        salt=salt
-    )
-
-    db.session.add(user)
-    db.session.commit()
+    user = create_user(data)
 
     return jsonify({'access_token': create_access_token({'id': user.id})}), 201
--- main/engines/user.py
+++ main/engines/user.py
@@ -1,5 +1,9 @@
+from typing import Dict
+
+from main import db
 from main.models.user import UserModel
 from main.libs.password_utils import check_password_hash
+from main.libs.password_utils import gen_salt, generate_password_hash
 
 
 def find_by_email(email: str) -> UserModel:
@@ -15,5 +19,21 @@ def find_by_email_and_password(email: str, password: str):
 
     if not user or not check_password_hash(user.password_hash, password, user.salt):
         return None
+
+    return user
+
+
+def create_user(data: Dict) -> UserModel:
+    salt = gen_salt()
+    password_hash = generate_password_hash(data['password'], salt)
+
+    user = UserModel(
+        email=data['email'],
+        password_hash=password_hash,
+        salt=salt
+    )
+
+    db.session.add(user)
+    db.session.commit()
 
     return user
