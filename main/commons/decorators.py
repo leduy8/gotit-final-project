e3e0a0f5ab5c18f3fef19b866d3e117ba5081ca3
Duy Le <duy.leduc8@gmail.com>
2022-03-08 09:52:21 +0700

    feat: Build decorator for auth jwt token

--- main/commons/decorators.py
+++ main/commons/decorators.py
@@ -1,6 +1,6 @@
 import logging
 import functools
-from main.commons.exceptions import Unauthorized
+from main.commons.exceptions import Forbidden, Unauthorized
 from main.libs.jwt import get_jwt_token, get_jwt_payload
 from main.engines.user import find_by_id
 
@@ -13,15 +13,11 @@ def jwt_required(f):
         if not token:
             raise Unauthorized(error_message='Token is required.')
 
-        try:
-            data = get_jwt_payload(token)
-            user = find_by_id(data['id'])
-            if not user:
-                raise
-            return f(user.id, *args, **kwargs)
-        except Exception as e:
-            logging.exception(e)
-            raise Unauthorized(error_message='Token is invalid or has been revoked.')
+        data = get_jwt_payload(token)
+        user = find_by_id(data['id']) if data else None
+        if not user:
+            raise Forbidden(error_message='Token is invalid or has been revoked.')
+        return f(user.id, *args, **kwargs)
 
     return decorated
 
@@ -34,14 +30,10 @@ def jwt_not_required(f):
         if not token:
             return f(None, *args, **kwargs)
 
-        try:
-            data = get_jwt_payload(token)
-            user = find_by_id(data['id'])
-            if not user:
-                raise
-            return f(user.id, *args, **kwargs)
-        except Exception as e:
-            logging.exception(e)
-            raise Unauthorized(error_message='Token is invalid or has been revoked.')
+        data = get_jwt_payload(token)
+        user = find_by_id(data['id'])
+        if not user:
+            raise
+        return f(user.id, *args, **kwargs)
 
     return decorated
