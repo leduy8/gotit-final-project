84132102379dfc6f0a30a397355fb29166b4e119
Duy Le <duy.leduc8@gmail.com>
2022-03-14 10:05:11 +0700

    fix: Modify user mail schema range

--- main/schemas/user.py
+++ main/schemas/user.py
@@ -5,14 +5,9 @@ from main.schemas.base import BaseSchema
 
 class UserSchema(BaseSchema):
     id = fields.Integer(dump_only=True)
-    email = fields.Email(
-        validate=validate.Length(max=50),
-        required=True
-    )
+    email = fields.Email(validate=validate.Length(max=254), required=True)
     password = fields.String(
-        load_only=True,
-        required=True,
-        validate=validate.Length(min=6, max=32)
+        load_only=True, required=True, validate=validate.Length(min=6, max=32)
     )
     salt = fields.String(dump_only=True)
     created = fields.DateTime(dump_only=True)
