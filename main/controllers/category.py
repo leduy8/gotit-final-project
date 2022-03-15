6deb4b7c0992bf6286964fdb28d07b4460d94e20
Duy Le <duy.leduc8@gmail.com>
2022-03-15 13:21:14 +0700

    fix: Fix for changes in decorator and engine

--- main/controllers/category.py
+++ main/controllers/category.py
@@ -1,87 +1,115 @@
-from flask import jsonify, request
+from typing import Dict, List
+
+from flask import jsonify
 
 from main import app
-from main.commons.decorators import require_authorized_user, require_non_authoried_user, validate_request
-from main.commons.exceptions import BadRequest, NotFound, Forbidden
+from main.commons.decorators import authorize_user, validate_request
+from main.commons.exceptions import BadRequest, Forbidden, NotFound
 from main.engines import category as category_engine
+from main.models.category import CategoryModel
 from main.schemas.base import PaginationSchema
 from main.schemas.category import CategorySchema
 
 
-@app.post('/categories')
-@require_authorized_user
-@validate_request('body', CategorySchema)
-def create_category(user_id):
-    data = request.get_json() or {}
+def get_category_data(category: CategoryModel, user_id: int) -> Dict:
+    return {
+        "id": category.id,
+        "name": category.name,
+        "created": category.created,
+        "updated": category.updated,
+        "is_owner": category.user_id == user_id,
+    }
 
-    if category_engine.find_category_by_name(data['name']):
-        raise BadRequest(
-            error_message=f'Category with name={data["name"]} has already been used.')
+
+def get_category_data_with_params_data(
+    categories: List[CategoryModel], user_id, *args, **kwargs
+):
+    return {
+        "categories": [get_category_data(category, user_id) for category in categories],
+        "page": kwargs["params"]["page"],
+        "items_per_page": kwargs["params"]["items_per_page"],
+        "total_items": kwargs["params"]["total_items"],
+    }
+
+
+@app.post("/categories")
+@authorize_user()
+@validate_request(CategorySchema)
+def create_category(data, user_id):
+    if category_engine.find_category_by_name(data["name"]):
+        raise BadRequest(error_message="Category name has already been used.")
 
     category = category_engine.create_category(data, user_id)
 
-    return CategorySchema().jsonify(category), 201
+    return CategorySchema().jsonify(category)
 
 
-@app.get('/categories')
-@require_non_authoried_user
-@validate_request('params', PaginationSchema)
-def get_categories(user_id):
+@app.get("/categories")
+@authorize_user(required=False)
+@validate_request(PaginationSchema)
+def get_categories(data, user_id):
     params = {
-        'page': request.args.get('page', 1, type=int),
-        'items_per_page': request.args.get('items_per_page', app.config['CATEGORIES_PER_PAGE'], type=int),
-        'total_items': category_engine.get_category_count()
+        "page": data["page"] if "page" in data else 1,
+        "items_per_page": data["items_per_page"]
+        if "items_per_page" in data
+        else app.config["CATEGORIES_PER_PAGE"],
+        "total_items": category_engine.get_category_count(),
     }
 
-    categories = category_engine.get_all_categories(params, user_id)
+    categories = category_engine.get_categories(params)
 
-    return jsonify(categories)
-
-
-@app.get('/categories/<int:id>')
-@require_non_authoried_user
-def get_category_by_id(user_id, id):
-    category = category_engine.get_category_by_id(id, user_id)
-
-    if not category:
-        raise NotFound(error_message=f'Category not found.')
-
-    return jsonify(category)
-
-
-@app.put('/categories/<int:id>')
-@require_authorized_user
-@validate_request('body', CategorySchema)
-def update_category_by_id(user_id, id):
-    category = category_engine.get_category_by_id(id, user_id)
-
-    if not category:
-        raise NotFound(error_message='Category not found.')
-
-    if not category['is_owner']:
-        raise Forbidden(error_message='Token is invalid or has been revoked.')
-
-    data = request.get_json()
-    updated_category = category_engine.update_category(data, id)
-
-    if not updated_category:
-        raise BadRequest(
-            error_message=f'Category with name={data["name"]} has already been used.'
+    return jsonify(
+        get_category_data_with_params_data(
+            categories=categories, user_id=user_id, params=params
         )
+    )
+
+
+@app.get("/categories/<int:id>")
+@authorize_user(required=False)
+def find_category_by_id(user_id, id):
+    category = category_engine.find_category_by_id(id)
+
+    if not category:
+        raise NotFound(error_message="Category not found.")
+
+    return jsonify(get_category_data(category, user_id))
+
+
+@app.put("/categories/<int:id>")
+@authorize_user()
+@validate_request(CategorySchema)
+def update_category_by_id(data, user_id, id):
+    category = category_engine.find_category_by_id(id)
+
+    if not category:
+        raise NotFound(error_message="Category not found.")
+
+    if category.user_id != user_id:
+        raise Forbidden(
+            error_message="User doesn't have permission to update this category"
+        )
+
+    if category_engine.find_category_by_name(data["name"]):
+        raise BadRequest(error_message="Category name has already been used.")
+
+    updated_category = category_engine.update_category(data, id)
 
     return CategorySchema().jsonify(updated_category)
 
 
-@app.delete('/categories/<int:id>')
-@require_authorized_user
+@app.delete("/categories/<int:id>")
+@authorize_user()
 def delete_category_by_id(user_id, id):
-    category = category_engine.get_category_by_id(id, user_id)
+    category = category_engine.find_category_by_id(id)
 
     if not category:
-        raise NotFound(error_message='Category not found.')
+        raise NotFound(error_message="Category not found.")
 
-    if not category['is_owner']:
-        raise Forbidden(error_message='Token is invalid or has been revoked.')
+    if category.user_id != user_id:
+        raise Forbidden(
+            error_message="User doesn't have permission to update this category"
+        )
 
     category_engine.delete_category(id)
 
--- main/engines/category.py
+++ main/engines/category.py
@@ -5,53 +5,29 @@ from main.models.category import CategoryModel
 
 
 def find_category_by_name(name: str) -> CategoryModel:
-    category = CategoryModel.query.filter_by(name=name).first()
-
-    if not category:
-        return None
-
-    return category
+    return CategoryModel.query.filter_by(name=name).first()
 
 
-def get_category_data(category: CategoryModel, user_id: int) -> Dict:
-    return {
-        'id': category.id,
-        'name': category.name,
-        'created': category.created,
-        'updated': category.updated,
-        'is_owner': category.user_id == user_id
-    }
+def find_category_by_id(id) -> Dict:
+    return CategoryModel.query.filter_by(id=id).first()
 
 
 def get_category_count() -> int:
     return CategoryModel.query.count()
 
 
-def get_all_categories(params: Dict, user_id: int) -> List[CategoryModel]:
+def get_categories(params: Dict) -> List[CategoryModel]:
     categories = CategoryModel.query.paginate(
-        params['page'], params['items_per_page'], False)
+        params["page"], params["items_per_page"], False
+    )
 
-    categories = [get_category_data(category, user_id) for category in categories.items]
+    categories = [category for category in categories.items]
 
-    return {
-        'categories': categories,
-        'page': params['page'],
-        'items_per_page': params['items_per_page'],
-        'total_items': params['total_items']
-    }
-
-
-def get_category_by_id(id, user_id) -> Dict:
-    category = CategoryModel.query.filter_by(id=id).first()
-
-    if not category:
-        return None
-
-    return get_category_data(category, user_id)
+    return categories
 
 
 def create_category(data: Dict, user_id) -> CategoryModel:
-    category = CategoryModel(name=data['name'], user_id=user_id)
+    category = CategoryModel(name=data["name"], user_id=user_id)
 
     db.session.add(category)
     db.session.commit()
@@ -60,12 +36,9 @@ def create_category(data: Dict, user_id) -> CategoryModel:
 
 
 def update_category(data, id) -> CategoryModel:
-    if CategoryModel.query.filter_by(name=data['name']).first():
-        return None
-
     category = CategoryModel.query.filter_by(id=id).first()
 
-    category.name = data['name']
+    category.name = data["name"]
 
     db.session.commit()
 
