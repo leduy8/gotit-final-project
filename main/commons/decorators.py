94018e26abb7b1c55bb1d2601a4cbf7f94d28d04
Duy Le <duy.leduc8@gmail.com>
2022-03-16 15:47:09 +0700

    fix: Add custom exceptions for details, rename decorator for more meaningful means

--- main/commons/decorators.py
+++ main/commons/decorators.py
@@ -4,21 +4,28 @@ import jwt
 from flask import request
 from marshmallow import ValidationError
 
-from main.commons.exceptions import BadRequest, Forbidden, Unauthorized
+from main.commons.exceptions import (
+    BadRequest,
+    InvalidAuthorizationError,
+    MissingAuthorzationError,
+    Unauthorized,
+)
 from main.engines.user import find_user_by_id
 from main.libs.jwt import get_jwt_payload, get_jwt_token
 from main.schemas.base import BaseSchema
 
 
-def authorize_user(required=True):
+def authenticate_user(required=True):
     def decorated(f):
         @functools.wraps(f)
         def wrapper(*args, **kwargs):
             try:
                 token = get_jwt_token()
-            except ValueError:
+            except (MissingAuthorzationError, InvalidAuthorizationError):
                 if required:
-                    raise Unauthorized(error_message="Token is required")
+                    raise Unauthorized(
+                        error_message="Token is either missing or invalid"
+                    )
                 return f(None, *args, **kwargs)
 
             try:
@@ -26,29 +33,27 @@ def authorize_user(required=True):
                 user = find_user_by_id(data["id"])
 
                 if not user:
-                    raise Forbidden(
-                        error_message="Token is invalid or has been revoked"
-                    )
+                    raise Unauthorized(error_message="Token is invalid")
 
                 return f(user.id, *args, **kwargs)
             except jwt.DecodeError:
-                raise Forbidden(error_message="Token is invalid or has been revoked")
+                raise Unauthorized(error_message="Token is invalid")
 
         return wrapper
 
     return decorated
 
 
-def validate_request(schema: BaseSchema):
+def pass_data(schema: BaseSchema):
     def decorated(f):
         @functools.wraps(f)
         def wrapper(*args, **kwargs):
             data = None
             try:
-                if request.method in ["POST", "PUT"]:
-                    data = schema().load(request.get_json())
-                elif request.method in ["GET"]:
+                if request.method in ["GET", "DELETE"]:
                     data = schema().load(request.args.to_dict())
+                else:
+                    data = schema().load(request.get_json())
             except ValidationError as e:
                 raise BadRequest(
                     error_data=e.messages, error_message="Invalid input value(s)"
--- main/libs/jwt.py
+++ main/libs/jwt.py
@@ -4,11 +4,12 @@ import jwt
 from flask import request
 
 from main import config
+from main.commons.exceptions import InvalidAuthorizationError, MissingAuthorzationError
 
 
-def create_access_token(payload: Dict) -> str:
+def create_access_token(payload: Dict, key=config.SECRET_KEY) -> str:
     if type(payload) == dict:
-        return jwt.encode(payload=payload, key=config.SECRET_KEY, algorithm="HS256")
+        return jwt.encode(payload=payload, key=key, algorithm="HS256")
 
     raise TypeError("Payload must be a dictionary")
 
@@ -17,12 +18,14 @@ def get_jwt_token() -> str:
     auth_header = request.headers.get("Authorization", None)
 
     if not auth_header:
-        raise ValueError("Authorization header not found")
+        raise MissingAuthorzationError("Authorization header not found")
 
     auth_header_split = auth_header.split(" ")
 
     if not auth_header.startswith("Bearer ") or len(auth_header_split) != 2:
-        raise ValueError("Authorization header should be a valid jwt token.")
+        raise InvalidAuthorizationError(
+            "Authorization header should be a valid jwt token."
+        )
 
     return auth_header_split[1]
 
